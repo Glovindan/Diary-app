571202c595705aaffd8edbf537977acdbc44123b
Glovindan Golovkin <turbo69nice@gmail.com>
2022-04-29 07:55:40 +0400

    started events display layout

--- client/diary/src/DayCalendar/DayCalendar.js
+++ client/diary/src/DayCalendar/DayCalendar.js
@@ -4,8 +4,16 @@ import styles from './DayCalendar.module.css'
 function DayCalendar() {
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.timelineContainer}>
         <Timeline/>
+      </div>
+
+      <div className={styles.eventsWrapper}>
+        <div className={styles.block}>
+          <p>8:40 - 10:30</p>
+          <p>Очень важная встреча с очень важными людьми</p>
+          <p>г.Ижевск ул.Майская д.23</p>
+        </div>
       </div>
     </div>
   );
